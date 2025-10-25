f2d78180d04439bea0f02dad5437014a262aa328
Nick Meyer <nmeyer@stackav.com>
2025-10-25 13:06:21 +0000

    Change fzf bindings to be more project focused.

--- init.lua
+++ init.lua
@@ -79,50 +79,84 @@ vim.keymap.set("n", "<leader>e", function()
   vim.api.nvim_feedkeys(string.format(":edit %s", buffer_dir), "n", true)
 end, { desc = "Edit from the relative directory." })
 
+-- Get project root if in one.  Returns nil if no project is recognized.
+local function get_project_root()
+  -- First check for git.
+  local job = vim.fn.systemlist(string.format("cd %s; git rev-parse --show-toplevel 2>/dev/null || true", get_buffer_dir()))
+  local git_root = job[1]
+  if git_root and git_root ~= "" then
+    return git_root
+  end
+  return nil
+end
 
 -- Find project files.  By default it tries to search from the root of the
 -- git repo.  If not in a repo, it searches from the current directory and
 -- down.
 local function find_project_files()
-  local buffer_dir = get_buffer_dir()
+  local project_root = get_project_root()
 
-  -- Get the directory from the buffer to use as the "current directory".
-  local job = vim.fn.systemlist(string.format("cd %s; git rev-parse --show-toplevel 2>/dev/null || true", buffer_dir))
-  local git_root = job[1]
-
-  if git_root and git_root ~= "" then
-    require("fzf-lua").git_files({ cwd = git_root })
+  if project_root and project_root ~= "" then
+    require("fzf-lua").git_files({ cwd = project_root })
   else
+    local buffer_dir = get_buffer_dir()
     -- Fallback to current working directory if not in a git repo
     require("fzf-lua").files({cwd = buffer_dir})
     vim.notify(string.format("Not in a Git repository. Searching from current directory: %s", buffer_dir), vim.log.levels.WARN)
   end
 end
 
--- Map to a keybinding.
-vim.keymap.set("n", "<leader>ff", find_project_files, { desc = "Find project files with FZF." })
+-- Find files in projecct.
+vim.keymap.set("n", "<leader>pf", find_project_files, { desc = "Find project files with FZF." })
 
--- Search through project files.  By default it tries to search from the root of the
--- git repo.  If not in a repo, it searches from the current directory and
--- down.
-local function search_project_files()
-  local buffer_dir = get_buffer_dir()
+-- Grep through the contents of project files.  It tries to search from the
+-- root of the git repo.  If not in a repo, it searches from the current
+-- directory and down.
+local function grep_project_files()
+  local git_root = get_project_root()
 
-  -- Get the directory from the buffer to use as the "current directory".
-  local job = vim.fn.systemlist(string.format("cd %s; git rev-parse --show-toplevel 2>/dev/null || true", buffer_dir))
-  local git_root = job[1]
-
+  -- Default to current working directory if not in a git repo
+  local dir_to_search = get_buffer_dir()
   if git_root and git_root ~= "" then
-    require("fzf-lua").live_grep({ cwd = git_root })
+    dir_to_search = git_root
   else
-    -- Fallback to current working directory if not in a git repo
-    require("fzf-lua").live_grep({cwd = buffer_dir})
     vim.notify(string.format("Not in a Git repository. Searching from current directory: %s", buffer_dir), vim.log.levels.WARN)
   end
+  require("fzf-lua").live_grep_native({ cwd = dir_to_search })
 end
 
--- Map to a keybinding.
-vim.keymap.set("n", "<leader>fs", search_project_files, { desc = "Search project files with FZF." })
+-- Grep files in project.
+vim.keymap.set("n", "<leader>ps", grep_project_files, { desc = "Grep project files with FZF." })
+
+local find_directory_cmd = "find . -name \".git\" -prune -o -type d -print"
+
+-- Find project directories.  By default it tries to search from the root of
+-- the git repo.  If not in a repo, it searches from the current directory and
+-- down.
+local function find_project_directories()
+  local git_root = get_project_root()
+
+  -- Default to current working directory if not in a git repo
+  local dir_to_search = get_buffer_dir()
+  if git_root and git_root ~= "" then
+    dir_to_search = git_root
+  else
+    vim.notify(string.format("Not in a Git repository. Searching from current directory: %s", buffer_dir), vim.log.levels.WARN)
+  end
+  require("fzf-lua").files({ cmd = find_directory_cmd, cwd = dir_to_search })
+end
+
+-- Find directories in project.
+vim.keymap.set("n", "<leader>pd", find_project_directories, { desc = "Find project directories with FZF." })
+
+-- Find files under current directory
+vim.keymap.set("n", "<leader>lf", function() require("fzf-lua").files({ search = get_buffer_dir(), cmd = "rg --files --sort path --max-depth 1 .", cwd = get_buffer_dir() }) end, { desc = "Find files in same directory" })
+
+-- Grep files under current directory
+vim.keymap.set("n", "<leader>ls", function() require("fzf-lua").live_grep_native({ cwd = get_buffer_dir() }) end, { desc = "Grep files in same directory" })
+
+-- Find directories under current directory.
+vim.keymap.set("n", "<leader>ld", function() require("fzf-lua").files({ search = get_buffer_dir(), cmd = find_directory_cmd, cwd = get_buffer_dir() }) end, { desc = "Find files in same directory" })
 
 -- Find open buffers
 vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers, { desc = "Find open buffers with FZF." })
