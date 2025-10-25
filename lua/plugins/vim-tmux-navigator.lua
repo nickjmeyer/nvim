7f892a2b184340a3ca9b502d7da527abfb8abff4
Nick Meyer <nmeyer@stackav.com>
2025-10-25 13:07:44 +0000

    Add vim tmux navigator plugin.

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,19 @@
+return {{
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<m-y>", "<cmd>TmuxNavigatePrevious<cr>" },
+  },
+}}
+
