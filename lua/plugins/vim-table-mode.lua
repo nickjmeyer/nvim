cfbd7dace00fe0b5fe813018efc56c1ce571bd6d
Nick Meyer <nmeyer@stackav.com>
2026-02-19 18:40:08 +0000

    Add table mode for markdown.

---
+++ lua/plugins/vim-table-mode.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "dhruvasagar/vim-table-mode",
+    -- Optional: lazy-load on filetype markdown or when the command is used
+    ft = "markdown", -- This loads the plugin for markdown files
+    cmd = "TableModelToggle", -- This loads the plugin when you run :TableModelToggle
+  },
+}
